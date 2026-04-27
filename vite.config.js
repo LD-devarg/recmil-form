5d5c361fe35b7406c92a745178369c84f0737c45
LD.dev <ld.devarg@gmail.com>
2026-04-27 12:25:30 -0300

    fix: base url recmil-form

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // Cambiá 'recmil-form' por el nombre exacto de tu repo en GitHub
 export default defineConfig({
   plugins: [react()],
-  base: '/recmil/',
+  base: '/recmil-form/',
 })
